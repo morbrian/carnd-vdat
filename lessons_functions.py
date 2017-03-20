c609b2fc41d90364c8e1faff5bca46881dca4f41
Brian Moriartty <morbrian@me.com>
2017-03-20 06:33:49 -0700

    saving current video (i forgot to commit when video started so I'm not sure if the history function was in generate_frame_heatmap or identify_raw_bboxes for this video)j white car fails to match as it gets further away, and there's still some false positives elsewhere.

--- lessons_functions.py
+++ lessons_functions.py
@@ -292,7 +292,7 @@ def find_cars(img, svc, X_scaler, ystart=400, ystop=704, scale=1,
             test_features = X_scaler.transform(np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))
             test_prediction = svc.predict(test_features)
 
-            if test_prediction == 1 or grid is True:
+            if test_prediction == 1 and svc.decision_function(test_features) > 0.4 or grid is True:
                 xbox_left = np.int(xleft*scale)
                 ytop_draw = np.int(ytop*scale)
                 win_draw = np.int(window*scale)
@@ -331,10 +331,12 @@ def nonoverlapping_bboxes(labels):
         nonzerox = np.array(nonzero[1])
         # Define a bounding box based on min/max x and y
         match_box = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
-        width = match_box[1][0] - match_box[0][0]
-        height = match_box[1][1] - match_box[0][1]
-        if width > 0 and height > 0:
-            bboxes.append(match_box)
+        # width = match_box[1][0] - match_box[0][0]
+        # height = match_box[1][1] - match_box[0][1]
+        # if match_box[1][0] > 570 and width > 64 and height > 64:
+        #     bboxes.append(match_box)
+        # elif match_box[1][0] <= 570:
+        bboxes.append(match_box)
 
     # Return the image
     return bboxes
